0e289141cc9fd3a41ce01f0ee47a0a6ca699e724
zimin.zhang <zimin.zhang@bentley.com>
2025-08-27 08:59:21 +0800

    Add bulk feature flag configuration script

---
+++ bulk_featureflag.bat
@@ -0,0 +1,37 @@
+@echo off
+
+set "civil-imodel-connector-cifconverterv2=0"
+set "save-ids-as-codes=0"
+set "2d-models-spatial=0"
+set "3d-elements-graphics=0"
+set "imodelbridge-repositorylink-useguid=0"
+set "open-v8-rootfile-once=1"
+set "reject-ecef-without-gcs=0"
+set "imbm-override-all-illformed-urns=0"
+set "imodelbridge_externalsource_codevalue_by_name=0"
+set "skip-drawings-and-sheets=0"
+set "convert-sheet-index=0"
+set "allow-hypermodeling=1"
+set "dgnv8-connector-use-descriptive-partition-name=0"
+set "dgnv8-connector-find-defModel-under-subject=0"
+set "imbm-minimal-CifTerrain-representation=0"
+set "filter-by-sheet-index=0"
+set "dgnv8-initialize-dwg-proxy-settings=1"
+
+setx civil-imodel-connector-cifconverterv2 0
+setx save-ids-as-codes 0
+setx 2d-models-spatial 0
+setx 3d-elements-graphics 0
+setx imodelbridge-repositorylink-useguid 0
+setx open-v8-rootfile-once 1
+setx reject-ecef-without-gcs 0
+setx imbm-override-all-illformed-urns 0
+setx imodelbridge_externalsource_codevalue_by_name 0
+setx skip-drawings-and-sheets 0
+setx convert-sheet-index 0
+setx allow-hypermodeling 1
+setx dgnv8-connector-use-descriptive-partition-name 0
+setx dgnv8-connector-find-defModel-under-subject 0
+setx imbm-minimal-CifTerrain-representation 0
+setx filter-by-sheet-index 0
+setx dgnv8-initialize-dwg-proxy-settings 1
